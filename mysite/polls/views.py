e1d1926f19827edbb54dee9c72aa23268c7aaf45
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-10-05 11:52:42 -0400

    Import library

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,7 +1,7 @@
 
 from django.http import HttpResponse
-from django.http import get_object_or_404, Http404
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
 #from django.template import loader
 
 from .models import Question
