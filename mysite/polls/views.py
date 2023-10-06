3503ad1620942bf6886f46f0cfe5f70de6251846
ll4407 <90568586+ll4407@users.noreply.github.com>
2023-10-06 10:22:31 -0400

    No comment

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -40,6 +40,7 @@ def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})'''
 
+
 # Using generic views
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
